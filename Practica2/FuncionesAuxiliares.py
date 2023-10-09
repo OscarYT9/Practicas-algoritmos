c4494c0d769d23602b32d7f8e254f4462aa2ac30
Oscar V.R <113378321+OscarYT9@users.noreply.github.com>
2023-10-09 20:28:29 +0000

    Update FuncionesAuxiliares.py and FuncionesTest.py

--- Practica2/FuncionesAuxiliares.py
+++ Practica2/FuncionesAuxiliares.py
@@ -1,17 +1,4 @@
 def aleatorio(n):
-    """
-     Parámetros
-     ----------
-     n: 
-        valor de tipo int que representa el tamaño del vector aleatorio
-
-     Qué hace la función?
-     ----------
-        Genera un vector de longitud n con números pseudoaleatorios en el rango [-n, n]
-     Devuelve
-     -------
-       El vector aleatorio generado
-    """
     
     import random
     v=list(range(n))                    # Crear una lista de números en el rango [0, n-1]
@@ -19,22 +6,8 @@ def aleatorio(n):
         v[i] = random.randint(-n, n)
     return v                            # Devolver la lista con valores aleatorios
 
-
 def vector_ordenado_aleatorio(n,orden):
-    """
-     Parámetros
-     ----------
-     n: 
-        valor de tipo int que reprsenta el tamaño del vector ordenado
 
-     Qué hace la función?
-     ----------
-        Genera un vector de longitud n con números aleatorios en el rango [-n, n], y luego lo ordena de forma ascendente.
-     Devuelve
-     -------
-       El vector ordenado con números aleatorios generado
-    """
-    
     v = aleatorio(n)  # Genera un vector de números aleatorios en el rango [-n, n]
     if orden =="ascendente":
       return sorted(v)  # Ordena el vector de forma ascendente
@@ -43,3 +16,40 @@ def vector_ordenado_aleatorio(n,orden):
     else:
        ValueError
 
+def calcular_tiempo_ejecucion(func, inicializar_func, vector, repeticiones_umbral):
+    
+   import time
+   inicio = time.perf_counter_ns()     # Registra el tiempo de inicio antes de ejecutar la función del algoritmo
+   func(vector)                        # Ejecuta la función del algoritmo para un vector dado
+   fin = time.perf_counter_ns()        # Registra el tiempo de finalización después de ejecutar la función
+   t= fin - inicio                     # Calcula y devuelve el tiempo transcurrido en nanosegundos
+
+   if t < 1000:
+        # Realizar repeticiones del algoritmo K veces
+        ta = time.perf_counter_ns()
+        for _ in range(repeticiones_umbral):
+            inicializar_func(vector)
+            func(vector)
+        tb = time.perf_counter_ns()
+        t1 = tb - ta
+        
+        ta = time.perf_counter_ns()
+        for _ in range(repeticiones_umbral):
+            inicializar_func(vector)
+        tb = time.perf_counter_ns()
+        t2 = tb - ta
+        
+        t = (t1 - t2) / repeticiones_umbral
+    
+   return t
+
+
+def cotas_ajustadas(n, tiempo, exp1, exp2, exp3):
+    
+   # Calcula las cotas subestimada, ajustada y sobrestimada en función de los parámetros dados
+    cota_subestimada= tiempo / (n ** exp1)
+    cota_ajustada = tiempo / (n ** exp2)
+    cota_sobrestimada = tiempo / (n ** exp3)
+
+    # Devuelve una cadena formateada con las cotas y el tamaño del vector
+    return f"{n:>10}\t\t{tiempo:>15.4f}       {cota_subestimada:>15.6f}{cota_ajustada:>15.6f}{cota_sobrestimada:>15.6f}"
--- Practica2/FuncionesTest.py
+++ Practica2/FuncionesTest.py
@@ -1,4 +1,6 @@
-from FuncionesAuxiliares import *
+#Funciones para comprobar los algoritmos
+from Algoritmos import *            #Importamos los algoritmos a probar
+from FuncionesAuxiliares import *   #Importamos las funciones auxiliares necesarias para la ejecución de las pruebas
 
 # 2. Valide el correcto funcionamiento de la implementación. La salida debería ser como sigue:
 
@@ -8,9 +10,9 @@ def ordenado(v):
             return False
     return True
 
-v = [1, 2]
-print(ordenado(v))  # True, la lista está ordenada ascendentemente
-print(aleatorio(17))
+#v = [1, 2]
+#print(ordenado(v))  # True, la lista está ordenada ascendentemente
+#print(aleatorio(17))
 #_______________________________________________________________________________________________________________________________________________________________________________________________________________________________
 
 
@@ -22,22 +24,22 @@ tamanos_n=[128, 256, 512, 1024, 2048, 4096, 8192, 16384] # Lista con los tamaño
 umbral_confianza = 1000                                                       
 repeticiones_umbral = 10 
 
-a =[1,2,3,4]
-b= [4,2,3,1]
+#a =[1,2,3,4]
+#b= [4,2,3,1]
 n = 10  # Cambia este valor según tus necesidades
 vector_asc = vector_ordenado_aleatorio(n,"ascendente")
 vector_desc = vector_ordenado_aleatorio(n,"descendente")
 vector_alet = aleatorio(n)
-print(vector_asc, vector_desc, vector_alet)
+#print(vector_asc, vector_desc, vector_alet)
 
 
 
-tamanos_n = [500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000] # Lista con los tamaños del vector aleatorio, es una progresión geométrica de razón 2, si se quisiese se podría automatizar su creación también, en este caso no lo hacemos ya que con esos valores deberia ser suficiente para comprobar la complejidad algorítmica
-umbral_confianza = 1000                                                        # El tiempo en us (microsegundos) en el cual no nos podemos fiar de los valores obtenidos y habrá que realizar varias iteraciones y hacer el promedio de las mismas para asegurarnos de obtener valores correctos.
-repeticiones_umbral = 10                                                       # Número iteraciones una vez superado el umbral de tiempo
+tamanos_n = [128, 256, 512, 1024, 2048, 4096, 8192, 16384]       # Lista con los tamaños del vector aleatorio, es una progresión geométrica de razón 2, si se quisiese se podría automatizar su creación también, en este caso no lo hacemos ya que con esos valores deberia ser suficiente para comprobar la complejidad algorítmica
+umbral_confianza = 1000                                          # El tiempo en us (microsegundos) en el cual no nos podemos fiar de los valores obtenidos y habrá que realizar varias iteraciones y hacer el promedio de las mismas para asegurarnos de obtener valores correctos.
+repeticiones_umbral = 10                                         # Número iteraciones una vez superado el umbral de tiempo
 
 #-------------------------------------------------------------------------------------------------------------------------
-def test_tiempo_complejidad(algoritmo, imprimir_solo_tiempo):
+def test_tiempo_complejidad(algoritmo, vector):
     """
      Parámetros
      ----------
@@ -56,20 +58,20 @@ def test_tiempo_complejidad(algoritmo, imprimir_solo_tiempo):
      -------
         los valores de los tiempos de ejecución en nanosegundos para cada n
     """
-        
+    
     # Comprobamos el valor del parámetro algoritmo y configuramos las variables en consecuencia
     if algoritmo == 1:
-        algoritmo_str = "***SumaSubMax1***"
+        algoritmo_str = "***ins_sort***"
         tamanos = tamanos_n  # Limitar tamaños para Algoritmo 1
-        sumaSubMax_func = sumaSubMax1
+        Ordenacion_func = ins_sort
         exp1=1.8
         exp2=2
         exp3=2.2
 
     elif algoritmo == 2:
-        algoritmo_str = "***SumaSubMax2***"
+        algoritmo_str = "***shell_sort_hibbard***"
         tamanos = tamanos_n
-        sumaSubMax_func = sumaSubMax2
+        Ordenacion_func = shell_sort_hibbard
         exp1=0.8
         exp2=1
         exp3=1.2
@@ -79,39 +81,20 @@ def test_tiempo_complejidad(algoritmo, imprimir_solo_tiempo):
 
     # Imprimimos el nombre del algoritmo actual y las cabeceras de las columnas
     print(algoritmo_str)
-    if imprimir_solo_tiempo == "Si":
-        print(f"{'Tamaño de n':>12}             {'Tiempo de ejecución (ns)':>15}")
 
-    elif imprimir_solo_tiempo == "No":
-        print(f"{'Subestimada':>64}{'Ajustada':>12}{'Sobreestimada':>15}")
-        print(f"{'n':>12}\t\t{'t(n) (ns)':>15}{'t(n)/n^'+str(exp1):>22}{'t(n)/n^'+str(exp2):>15}{'t(n)/n^'+str(exp3):>15}")
-
-    else:
-        raise ValueError("Debes elegir si solo quieres imprimir el tiempo de ejecución o el tiempo + las cotas ['Si'/'No']")
+    print(f"{'Subestimada':>64}{'Ajustada':>12}{'Sobreestimada':>15}")
+    print(f"{'n':>12}\t\t{'t(n) (ns)':>15}{'t(n)/n^'+str(exp1):>22}{'t(n)/n^'+str(exp2):>15}{'t(n)/n^'+str(exp3):>15}")
 
     # Iteramos sobre diferentes tamaños de entrada
-    for n in tamanos:
-        vector = aleatorio(n)
-        vector_asc
-        vector_desc                                                                             #Creamos un vector de ese tamaño de entrada
-        tiempo_ejecucion = calcular_tiempo_ejecucion (sumaSubMax_func,vector)                               # Calculamos el tiempo de ejecución del algoritmo para ese vector
+    for n in tamanos:                                                                                       #Creamos un vector de ese tamaño de entrada
+        tiempo_ejecucion = calcular_tiempo_ejecucion (Ordenacion_func,vector)                               # Calculamos el tiempo de ejecución del algoritmo para ese vector
         
         if tiempo_ejecucion < umbral_confianza * 1000:                                                      # Comprobamos si el tiempo de ejecución está por debajo de un umbral de confianza (y pasamos el umbral de us a ns)
-            tiempo_promedio = calcular_tiempo_promedio(sumaSubMax_func, vector, repeticiones_umbral)        # Calculamos el tiempo promedio para varias repeticiones
-
-
-            if imprimir_solo_tiempo == "Si":
-                print(f"* {n:>10}\t\t{tiempo_promedio:>15.4f} (promedio de {repeticiones_umbral} repeticiones)")                        # Imprimimos el resultado con un asterisco para indicar el promedio
             
-            elif imprimir_solo_tiempo == "No":
-                print("*",cotas_ajustadas(n, tiempo_promedio, exp1, exp2, exp3),f"(promedio de {repeticiones_umbral} repeticiones)")    # Imprimimos el resultado con un asterisco para indicar el promedio
-
+            tiempo_promedio = calcular_tiempo_promedio(Ordenacion_func, vector, repeticiones_umbral)        # Calculamos el tiempo promedio para varias repeticiones
+            print("*",cotas_ajustadas(n, tiempo_promedio, exp1, exp2, exp3),f"(promedio de {repeticiones_umbral} repeticiones)")    # Imprimimos el resultado con un asterisco para indicar el promedio
 
         else:
-            if imprimir_solo_tiempo == "Si":
-                print(f"  {n:>10}\t\t{tiempo_ejecucion:>15.4f}")                                # Imprimimos el tiempo de ejecución normal
-
-            elif imprimir_solo_tiempo == "No":
-                print(" ",cotas_ajustadas(n, tiempo_ejecucion, exp1, exp2, exp3))               # Imprimimos el tiempo de ejecución normal
+            print(" ",cotas_ajustadas(n, tiempo_ejecucion, exp1, exp2, exp3))               # Imprimimos el tiempo de ejecución normal
 
 # Realmente esta última función podría simplificarse si se imprimiese el tiempo + cotas, pero como en el apartado 3. de la práctica pide exclusivamente el tiempo de ejecución hemos decidido incluir la opción de poder elegir si se quiere imprimir solo el tiempo, o el tiempo + las cotas para mayor comodidad
