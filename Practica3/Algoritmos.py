2979991aac667aa69f8be7ff9df9c7c1fb1bca59
Oscar V.R <113378321+OscarYT9@users.noreply.github.com>
2023-11-04 21:39:30 +0000

    Update Algoritmos.py and main.py (Clase Monticulo y Metodos implementados)

--- Practica3/Algoritmos.py
+++ Practica3/Algoritmos.py
@@ -1,73 +1,90 @@
-
-
-# Uso del código:
 class Monticulo:
-    def __init__(self, tamano_maximo):
-        self.Tamano_monticulo = tamano_maximo
-        self.Vector_monticulo = [None] * (tamano_maximo + 1)
+    def __init__(self):
+        self.Vector_monticulo = []
+        self.Tamano_monticulo = 0
 
     def __str__(self):
-        return f"Tamaño montículo: {self.Tamano_monticulo}, Vector montículo: {self.Vector_monticulo}"
-#______________________________________________________________________________________________________________________
-
-# def crear_monticulo(v: list, M: Monticulo):
-#     M.Vector_monticulo = v
-#     M.Tamaño_monticulo = len(v)
-#     i = M.Tamaño_monticulo/2
-#     for i in range(-1,1):
-#         __hundir(M,i)
-#     return M.Vector_monticulo
-
-
-def crear_Monticulo(v: list, M: Monticulo):
-    M.Vector_monticulo = v.copy()  # Copia los elementos de v a M.Vector_monticulo
-    M.Tamano_monticulo = len(v)
+        return f"Tamaño montículo: {self.Tamano_monticulo}, Vector montículo: {self.Vector_monticulo[1:]}"
     
-    for i in range(M.Tamano_monticulo // 2, 0, -1):
-        __hundir(M, i)
+    def crear_Monticulo(self, v):
+        self.Vector_monticulo = [0] + v
+        self.Tamano_monticulo = len(v)
 
-    return M.Vector_monticulo
+        for i in range(self.Tamano_monticulo // 2, 0, -1):
+            self.__hundir(i)
 
-def consultar_menor(M: Monticulo):
-    if M.Tamano_monticulo == 0:
-        return None
-    
-    return M.Vector_monticulo[1]
+    def consultar_menor(self):
+        if self.Tamano_monticulo == 0:
+            return None
 
-# def quitarMenor(M: Monticulo):
-#     if M.Tamano_monticulo == 0:
-#         return None
-    
-#     menor = M.Vector_monticulo[1]
-#     M.Vector_monticulo[1] = M.Vector_monticulo[M.Tamano_monticulo]
-#     M.Tamano_monticulo -= 1
-#     __hundir(M, 1)
-    
-#     return menor
+        return self.Vector_monticulo[1]
 
-#______________________________________
-def __hundir(M: Monticulo, i):
-    while True:
-        HijoIzq = 2 * i
-        HijoDer = 2 * i + 1
-        j = i
+    def quitarMenor(self):
+        if self.Tamano_monticulo == 0:
+            return None
 
-        if HijoDer <= M.Tamano_monticulo and M.Vector_monticulo[HijoDer - 1] < M.Vector_monticulo[i - 1]:
-            i = HijoDer
+        menor = self.consultar_menor()  # Utiliza la función consultar_menor
+        self.Vector_monticulo[1] = self.Vector_monticulo[self.Tamano_monticulo]
+        self.Tamano_monticulo -= 1
+        self.Vector_monticulo.pop()
+        self.__hundir(1)
 
-        if HijoIzq <= M.Tamano_monticulo and M.Vector_monticulo[HijoIzq - 1] < M.Vector_monticulo[i - 1]:
-            i = HijoIzq
+        return menor
 
-        if j == i:
-            break
-        else:
-            M.Vector_monticulo[j - 1], M.Vector_monticulo[i - 1] = M.Vector_monticulo[i - 1], M.Vector_monticulo[j - 1]
+    def __hundir(self, i):
+        while True:
+            HijoIzq = 2 * i
+            HijoDer = 2 * i + 1
+            j = i
 
+            if HijoDer <= self.Tamano_monticulo and self.Vector_monticulo[HijoDer] < self.Vector_monticulo[i]:
+                i = HijoDer
 
-v=[12, 11, 13, 5, 6, 7]
-M = Monticulo(len(v))
+            if HijoIzq <= self.Tamano_monticulo and self.Vector_monticulo[HijoIzq] < self.Vector_monticulo[i]:
+                i = HijoIzq
 
-print(crear_Monticulo(v, M))
-print(consultar_menor(M))
-# print(quitarMenor(M))
-# print(M)
+            if j != i:
+                self.Vector_monticulo[j], self.Vector_monticulo[i] = self.Vector_monticulo[i], self.Vector_monticulo[j]
+            else:
+                break
+
+# Crear un montículo
+mi_monticulo = Monticulo()
+print(mi_monticulo)
+
+# Agregar elementos al montículo
+elementos = [9, 5, 6, 2, 3]
+mi_monticulo.crear_Monticulo(elementos)
+print(mi_monticulo)
+
+menor_eliminado = mi_monticulo.quitarMenor()
+print(f"El menor elemento eliminado es: {menor_eliminado}")
+print(mi_monticulo)
+
+menor = mi_monticulo.quitarMenor()
+print(f"El menor elemento eliminado es: {menor}")
+print(mi_monticulo)
+
+menor_nuevo = mi_monticulo.quitarMenor()
+print(f"El menor elemento eliminado es: {menor_nuevo}")
+print(mi_monticulo)
+
+menor_eliminado = mi_monticulo.quitarMenor()
+print(f"El menor elemento eliminado es: {menor_eliminado}")
+print(mi_monticulo)
+
+menor_eliminado = mi_monticulo.quitarMenor()
+print(f"El menor elemento eliminado es: {menor_eliminado}")
+print(mi_monticulo)
+
+menor_eliminado = mi_monticulo.quitarMenor()
+print(f"El menor elemento eliminado es: {menor_eliminado}")
+print(mi_monticulo)
+
+menor_eliminado = mi_monticulo.quitarMenor()
+print(f"El menor elemento eliminado es: {menor_eliminado}")
+print(mi_monticulo)
+
+menor_eliminado = mi_monticulo.quitarMenor()
+print(f"El menor elemento eliminado es: {menor_eliminado}")
+print(mi_monticulo)
--- Practica3/main.py
+++ Practica3/main.py
@@ -1,14 +1,136 @@
-from ALgoritmos import *
-from FuncionesAuxiliares import *
-from FuncionesTest import *
+# from ALgoritmos import *
+# from FuncionesAuxiliares import *
+# from FuncionesTest import *
 
 
-v=[1,2,3,4,3,53,53,2,4,32,35,36]
-tamaño_maximo=len(v)
+# v=[1,2,3,4,3,53,53,2,4,32,35,36]
+# tamaño_maximo=len(v)
 
-crear_monticulo(v,Monticulo(v))
+# crear_monticulo(v,Monticulo(v))
 
-v = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
-M = Monticulo(11)
-crear_Monticulo(v, M)
-print(M.Vector_monticulo)
+# v = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+# M = Monticulo(11)
+# crear_Monticulo(v, M)
+# print(M.Vector_monticulo)
+
+# class MinHeap:
+#     def __init__(self):
+#         self.heap = []
+
+#     def parent(self, i):
+#         return (i - 1) // 2
+
+#     def left_child(self, i):
+#         return 2 * i + 1
+
+#     def right_child(self, i):
+#         return 2 * i + 2
+
+#     def insert(self, value):
+#         self.heap.append(value)
+#         i = len(self.heap) - 1
+#         while i > 0 and self.heap[i] < self.heap[self.parent(i)]:
+#             self.heap[i], self.heap[self.parent(i)] = self.heap[self.parent(i)], self.heap[i]
+#             i = self.parent(i)
+
+#     def extract_min(self):
+#         if not self.heap:
+#             return None
+#         root = self.heap[0]
+#         self.heap[0] = self.heap[-1]
+#         self.heap.pop()
+#         self._heapify(0)
+#         return root
+
+#     def _heapify(self, i):
+#         left = self.left_child(i)
+#         right = self.right_child(i)
+#         smallest = i
+#         if left < len(self.heap) and self.heap[left] < self.heap[smallest]:
+#             smallest = left
+#         if right < len(self.heap) and self.heap[right] < self.heap[smallest]:
+#             smallest = right
+#         if smallest != i:
+#             self.heap[i], self.heap[smallest] = self.heap[smallest], self.heap[i]
+#             self._heapify(smallest)
+
+# # Ejemplo de uso
+# heap = MinHeap()
+# heap.insert(5)
+# heap.insert(3)
+# heap.insert(8)
+# heap.insert(1)
+
+# print("Elementos extraídos en orden ascendente:")
+# while heap.heap:
+#     print(heap.extract_min())
+
+class MonticuloBinario:
+    def __init__(self):
+        self.listaMonticulo = [0]
+        self.tamanoActual = 0
+
+    def __str__(self):
+        return f"Tamaño montículo: {self.tamanoActual}, Vector montículo: {self.listaMonticulo}"
+
+    def infiltArriba(self,i):
+        while i // 2 > 0:
+          if self.listaMonticulo[i] < self.listaMonticulo[i // 2]:
+             tmp = self.listaMonticulo[i // 2]
+             self.listaMonticulo[i // 2] = self.listaMonticulo[i]
+             self.listaMonticulo[i] = tmp
+          i = i // 2
+
+    def insertar(self,k):
+      self.listaMonticulo.append(k)
+      self.tamanoActual = self.tamanoActual + 1
+      self.infiltArriba(self.tamanoActual)
+
+    def infiltAbajo(self,i):
+      while (i * 2) <= self.tamanoActual:
+          hm = self.hijoMin(i)
+          if self.listaMonticulo[i] > self.listaMonticulo[hm]:
+              tmp = self.listaMonticulo[i]
+              self.listaMonticulo[i] = self.listaMonticulo[hm]
+              self.listaMonticulo[hm] = tmp
+          i = hm
+
+    def hijoMin(self,i):
+      if i * 2 + 1 > self.tamanoActual:
+          return i * 2
+      else:
+          if self.listaMonticulo[i*2] < self.listaMonticulo[i*2+1]:
+              return i * 2
+          else:
+              return i * 2 + 1
+
+    def eliminarMin(self):
+      valorSacado = self.listaMonticulo[1]
+      self.listaMonticulo[1] = self.listaMonticulo[self.tamanoActual]
+      self.tamanoActual = self.tamanoActual - 1
+      self.listaMonticulo.pop()
+      self.infiltAbajo(1)
+      return valorSacado
+
+    def construirMonticulo(self,unaLista):
+      i = len(unaLista) // 2
+      self.tamanoActual = len(unaLista)
+      self.listaMonticulo = [0] + unaLista[:]
+      while (i > 0):
+          self.infiltAbajo(i)
+          i = i - 1
+
+miMonticulo = MonticuloBinario()
+miMonticulo.construirMonticulo([9,5,6,2,3])
+print(miMonticulo)
+
+print(miMonticulo.eliminarMin())
+print(miMonticulo)
+print(miMonticulo.eliminarMin())
+print(miMonticulo)
+print(miMonticulo.eliminarMin())
+print(miMonticulo)
+print(miMonticulo.eliminarMin())
+print(miMonticulo)
+print(miMonticulo.eliminarMin())
+print(miMonticulo)
