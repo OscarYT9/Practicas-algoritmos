07b03699fb4928ee2ad5a9886150c297ac25b84c
Oscar V.R <113378321+OscarYT9@users.noreply.github.com>
2023-11-05 02:08:04 +0000

    Update 5 files (crearMonticulo complejidad)

--- Practica3/FuncionesAuxiliares.py
+++ Practica3/FuncionesAuxiliares.py
@@ -15,11 +15,45 @@ def calcular_tiempo_ejecucion(n):    # Medir el tiempo de ejecución de una func
     import time
     mi_monticulo = Monticulo()
     vector = inicializar(n)  # Puedes usar el vector aleatorio como entrada
+
     inicio = time.perf_counter_ns()
     mi_monticulo.crear_Monticulo(vector)
     fin = time.perf_counter_ns()
+
     tiempo_ejecucion = fin - inicio
+
     return tiempo_ejecucion
+
+
+
+def calcular_tiempo_promedio(repeticiones_umbral, n):    # Realizar repeticiones de inicialización + algoritmo K veces, es decir, repetir el cálculo del algoritmo K veces y para eso necesitas inicializar el vector K veces también, sino inicializas el vector, el algoritmo siempre iteraría las K veces sobre el vector ordenado
+   import time
+
+   # Mide el tiempo de ejecución total de Ordenacion_func ejecutado en un vector inicializado repeticiones_umbral veces
+
+   ta = time.perf_counter_ns()                  # Tiempo antes de comenzar la ejecución
+   for _ in range(repeticiones_umbral):         # Inicializa un vector y ejecuta el algoritmo de ordenación en ese vector repeticiones_umbral veces
+      mi_monticulo = Monticulo()
+      vector = inicializar(n)         # Ejecuta el algoritmo en esa iteración (n) un numero k de veces para cerciorarte de que el algoritmo es correcto, por eso tienes que pasarle a esta función todo lo necesario para volver a ejecutar esa misma ejecución n numero de veces, medir su tiempo y hacer la media del tiempo de las diferentes ejecuciones
+      mi_monticulo.crear_Monticulo(vector)       
+   tb = time.perf_counter_ns()                  # Tiempo después de finalizar todas las ejecuciones
+
+   t1 = tb - ta                                 # Tiempo total de la ejecución = Tiempo al finalizar todas las ejecuciones - Tiempo antes de comenzar la ejecución
+
+
+   # Mide el tiempo de inicialización sola del vector (sin ejecutar el algoritmo) también repetido repeticiones_umbral veces
+
+   ta = time.perf_counter_ns()                  # Tiempo antes de comenzar la ejecución
+   for _ in range(repeticiones_umbral):         # Realiza un bucle for que inicializa un vector repeticiones_umbral veces
+       mi_monticulo = Monticulo()
+       vector = inicializar(n)
+   tb = time.perf_counter_ns()                  # Tiempo después de finalizar todas las ejecuciones
+   
+   t2 = tb - ta                                 # Tiempo de inicialización
+
+   t = (t1 - t2) / repeticiones_umbral          # Resta el tiempo de inicialización para almacenar solo con el del algoritmo y después lo divides entre las K repeticiones para obtener el promedio del tiempo necesario para ejecutar el algoritmo
+   return t       
+
 
 def cotas_ajustadas(n, tiempo, exp1, exp2, exp3):   # Calcula las cotas  
     
--- Practica3/FuncionesTests.py
+++ Practica3/FuncionesTests.py
@@ -5,10 +5,10 @@ from FuncionesAuxiliares import *   #Importamos las funciones auxiliares necesar
 
 tamanos_n = [500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000,512000,1024000]   # Lista con los tamaños del vector aleatorio, es una progresión geométrica de razón 2, si se quisiese se podría automatizar su creación también, en este caso no lo hacemos ya que con esos valores deberia ser suficiente para comprobar la complejidad algorítmica
 umbral_confianza = 1000                                          # El tiempo en us (microsegundos) en el cual no nos podemos fiar de los valores obtenidos y habrá que realizar varias iteraciones y hacer el promedio de las mismas para asegurarnos de obtener valores correctos.
-repeticiones_umbral = 10                                         # Número iteraciones una vez superado el umbral de tiempo
+repeticiones_umbral = 1000                                         # Número iteraciones una vez superado el umbral de tiempo
 
 #-------------------------------------------------------------------------------------------------------------------------
-def test_tiempo_complejidad(func_type, exp1, exp2, exp3):
+def test_tiempo_complejidad(exp1, exp2, exp3):
     '''
     Permite comprobar el tiempo de ejecución del algoritmo para cada caso del vector y además nos ayuda a demostrar su complejidad de manera empírica gracias al cálculo del tiempo de ejecución entre las cotas.
     '''
@@ -21,4 +21,11 @@ def test_tiempo_complejidad(func_type, exp1, exp2, exp3):
     for n in tamanos_n:
         
         tiempo_ejecucion = calcular_tiempo_ejecucion(n)  # Calculamos el tiempo de ejecución del algoritmo para ese vector
-        print(" ",cotas_ajustadas(n, tiempo_ejecucion, exp1, exp2, exp3))    # Imprimimos el tiempo de ejecución normal
+
+        if tiempo_ejecucion < umbral_confianza * 1000:                          # Comprobamos si el tiempo de ejecución está por debajo de un umbral de confianza (y pasamos el umbral de us a ns)
+
+            tiempo_promedio = calcular_tiempo_promedio(repeticiones_umbral, n)                           # Calculamos el tiempo promedio para varias repeticiones
+            print("*",cotas_ajustadas(n, tiempo_promedio, exp1, exp2, exp3),f"(promedio de {repeticiones_umbral} repeticiones)")    # Imprimimos el resultado con un asterisco para indicar el promedio
+
+        else:
+            print(" ",cotas_ajustadas(n, tiempo_ejecucion, exp1, exp2, exp3))    # Imprimimos el tiempo de ejecución normal
