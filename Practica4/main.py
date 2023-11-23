4f7ed1321904fd8122c042c7339dcb0cda736aa0
ainhoadds30 <145484102+ainhoadds30@users.noreply.github.com>
2023-11-23 12:32:33 +0100

    comentada

--- Practica4/main.py
+++ Practica4/main.py
@@ -4,7 +4,10 @@ from FuncionesTest import *
 
 
 if __name__ == "__main__":
+    #ejecuta el primer test, el que comprueba que los resultados después de aplicar el algoritmo dijkstra son correctos
     test_matrices_minimas()
+    
+    #10 iteraciones donde se muestra el tiempo de ejecución (t(n)) y t(n)/n^(cota)
     for i in range(10):
-        imprimir_complejidad(1.8,3,3.2)#2.7, 2.8
+        imprimir_complejidad(2.8,2.95,3.1)
      
